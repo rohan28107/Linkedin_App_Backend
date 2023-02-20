ddaaadf226aef35f563a6256138b5d5b162bdfe7
rohan28107 <rohangajre21@gmail.com>
2023-02-20 17:20:35 +0530

    backend 3

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ require('dotenv').config();
 const {userRouter} = require("./routes/User.routes");
 const {postRouter} = require('./routes/Posts.routes'); 
 const {authenticate} = require("./middlewares/authenticate.middleware");
-const cors = require('cors');
+// const cors = require('cors');
 
 
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 
 app.use("/users", userRouter);
